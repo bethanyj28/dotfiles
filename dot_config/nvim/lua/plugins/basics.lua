e29c7c50e12f1267a7e3a4dde477231b77d110f4
bethanyj28 <bethanyjanos28@gmail.com>
2023-10-11 17:28:56 -0400

    use updated indent_blankline setup

--- dot_config/nvim/lua/plugins/basics.lua
+++ dot_config/nvim/lua/plugins/basics.lua
@@ -3,9 +3,10 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
-			require('indent_blankline').setup {
-				char = '┊',
-				show_trailing_blankline_indent = false,
+			require('ibl').setup {
+				indent = {
+					char = '┊',
+				}
 			}
 		end,
 	},
