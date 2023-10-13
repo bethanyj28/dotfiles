4029692cb13764618ad5f254fea149062bdf3a82
bethanyj28 <bethanyjanos28@gmail.com>
2023-10-12 21:38:17 -0400

    separate copilot

--- dot_config/nvim/lua/plugins/autocomplete.lua
+++ dot_config/nvim/lua/plugins/autocomplete.lua
@@ -1,11 +1,11 @@
 return {
+	{ "github/copilot.vim" },
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
-			"github/copilot.vim",
 		},
 		event = "InsertEnter",
 		config = function()
