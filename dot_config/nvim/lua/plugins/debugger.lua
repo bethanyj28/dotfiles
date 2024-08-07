b9eab68bfb8d16304b5e7b3d8f27c6971d63ba05
bethanyj28 <bethanyjanos28@gmail.com>
2024-08-07 10:03:26 -0400

    use telescope for all the things

---
+++ dot_config/nvim/lua/plugins/debugger.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "williamboman/mason.nvim",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    config = function()
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      vim.keymap.set("n", "<leader>dd", dap.continue)
+			vim.keymap.set("n", "<leader>dl", dap.step_over)
+			vim.keymap.set("n", "<leader>dj", dap.step_into)
+			vim.keymap.set("n", "<leader>dk", dap.step_out)
+			vim.keymap.set("n", "<leader>dh", dap.step_back)
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  }
+}
